0b8e24d76988f8c1994b1a589291cdbd96b06703
Ayrton Denner <ayrtondenner_2013@hotmail.com>
2018-02-11 14:17:05 -0200

    Tentativa de correção do ruído nas imagens

--- gan_network_training/floorplan_resize.py
+++ gan_network_training/floorplan_resize.py
@@ -13,15 +13,16 @@ RESIZE_WIDTH = 64
 RESIZE_HEIGHT = 128
 REAL_IMAGE_PATH = 'input/real/'
 RESIZED_IMAGE_PATH = 'input/resized/'
+SHOW_FINAL_RESULT = False
 
 image_list = []
 kernel = np.ones((5,5), np.uint8)
 
 print("Lendo imagens...")
 
-for filename in glob.glob(REAL_IMAGE_PATH + '*.*'):
+for filepath in glob.glob(REAL_IMAGE_PATH + '*.*'):
 
-    image = cv.imread(filename, cv.IMREAD_COLOR)
+    image = cv.imread(filepath, cv.IMREAD_COLOR)
 
     # RGB 2 GRAY
     image = cv.cvtColor(image, cv.COLOR_BGR2GRAY)
@@ -35,9 +36,17 @@ for filename in glob.glob(REAL_IMAGE_PATH + '*.*'):
     # RESIZE
     image = cv.resize(image, (RESIZE_WIDTH, RESIZE_HEIGHT))
 
-    cv.imwrite(RESIZED_IMAGE_PATH + filename.split('\\')[1], image)
+    filename = filepath.split('\\')[1]
 
-    x = 0
+    cv.imwrite(RESIZED_IMAGE_PATH + filename, image)
+
+    #saved_image = cv.imread(RESIZED_IMAGE_PATH + filename, cv.IMREAD_GRAYSCALE)
+
+    #denoised_image = cv.fastNlMeansDenoising(saved_image, None, 10, 7, 21)
+
+    #cv.imwrite(RESIZED_IMAGE_PATH + filename, denoised_image)
+
+    #x = 0
 
 print("Imagens lidas")
 print("\n\n\n")
@@ -46,3 +55,21 @@ fim = datetime.datetime.now()
 print("Fim: " + str(fim) + '\n')
 
 print("Duração: " + str(fim - inicio))
+
+if SHOW_FINAL_RESULT:
+
+    for filepath in glob.glob(RESIZED_IMAGE_PATH + '*.*'):
+
+        saved_image = cv.imread(filepath, cv.IMREAD_GRAYSCALE)
+
+        denoised_image = cv.fastNlMeansDenoising(saved_image, None, 10, 7, 21)
+
+        figure = plt.figure(figsize=(2, 2))
+
+        figure.add_subplot(1, 1, 1)
+        plt.imshow(saved_image, 'gray')
+
+        figure.add_subplot(1, 2, 2)
+        plt.imshow(denoised_image, 'gray')
+
+        plt.show()
